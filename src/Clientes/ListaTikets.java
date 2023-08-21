ff8ba861fe560df9281dc2915db8c5e29c74be34
pablorod <jorodriguezce@est.utn.ac.cr>
2023-08-21 14:18:22 -0600

    se crean las clases
    se crean las clases y los atributos

---
+++ src/Clientes/ListaTikets.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clientes;
+
+/**
+ *
+ * @author Pablo Rod
+ */
+public class ListaTikets extends Tickets {
+
+    public ListaTikets(int numero, char tipo) {
+        super(numero, tipo);
+    }
+    
+    
+    public boolean agregar(Tickets ticket){
+    ticket.add();
+    }
+    public boolean eliminar(Tickets ticket{
+    ticket.delete(ticket);
+    }
+    public boolean buscar(Tickets ticket){
+    
+    }
+    
+    
+}
---
+++ src/Clientes/Tickets.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clientes;
+
+/**
+ *
+ * @author Pablo Rod
+ */
+public class Tickets {
+   private int numero;
+   private char tipo;
+   
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public Tickets(int numero, char tipo) {
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+   
+   
+   
+   
+}
